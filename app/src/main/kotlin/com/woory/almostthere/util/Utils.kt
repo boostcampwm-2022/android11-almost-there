8243cd5d60e4a46b6124510878ab0fd7bb693953
swjw13 <swjw13@gmail.com>
2022-11-16 17:34:41 +0900

    :sparkles: feat: 화면 이동 추가
    - 메인 화면 추가, 이동 구현
    - 화면 이동시에 애니메이션 구현
    
    Issue #5, #8

--- app/src/main/kotlin/com/woory/almostthere/util/Utils.kt
+++ app/src/main/kotlin/com/woory/almostthere/util/Utils.kt
@@ -1,2 +1,18 @@
 package com.woory.almostthere.util
 
+import androidx.navigation.NavOptions
+import com.woory.almostthere.R
+
+val animRightToLeftNavOption = NavOptions.Builder().apply {
+    setEnterAnim(R.anim.slide_in_from_right)
+    setExitAnim(R.anim.slide_out_to_left)
+    setPopEnterAnim(R.anim.slide_in_from_left)
+    setPopExitAnim(R.anim.slide_out_to_right)
+}.build()
+
+val animLeftToRightNavOptions = NavOptions.Builder().apply {
+    setEnterAnim(R.anim.slide_in_from_left)
+    setExitAnim(R.anim.slide_out_to_right)
+    setPopEnterAnim(R.anim.slide_in_from_right)
+    setPopExitAnim(R.anim.slide_out_to_left)
+}.build()
