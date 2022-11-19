1c26c31b11236b8449be80210922bce729b655b0
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-11-19 23:41:30 +0900

    :lipstick: design: actionbar 추가 및 activity navigation 수정
    - actionbar 추가
    - activity 이동 시 Intent 사용
    
    Issues #21

--- app/src/main/kotlin/com/woory/almostthere/ui/creatingpromise/CreatingPromiseActivity.kt
+++ app/src/main/kotlin/com/woory/almostthere/ui/creatingpromise/CreatingPromiseActivity.kt
@@ -1,5 +1,7 @@
 package com.woory.almostthere.ui.creatingpromise
 
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.ViewModelProvider
 import com.woory.almostthere.R
 import com.woory.almostthere.databinding.ActivityCreatingPromiseBinding
@@ -13,4 +15,23 @@ class CreatingPromiseActivity :
     private val viewModel: CreatingPromiseViewModel by lazy {
         ViewModelProvider(this)[CreatingPromiseViewModel::class.java]
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpAppBar()
+        setUpBackPressedCallback()
+    }
+
+    private fun setUpAppBar() {
+        binding.containerToolbar.toolbar.title = getString(R.string.promise_creation)
+    }
+
+    private fun setUpBackPressedCallback() {
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right)
+            }
+        })
+    }
+
 }
