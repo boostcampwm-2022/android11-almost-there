180ab3450b57fb0fdab437f37cd5ad6c719da9fe
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-11-19 17:03:13 +0900

    :wrench: chore: room, retrofit 종속성 추가
    - network 모듈에 retrofit 종속성 추가
    - database 모듈에 room 종속성 추가
    
    Issues #18
    
    Co-authored-by: swjw13 <swjw13@gmail.com>, fbghgus123 <fbghgus123@naver.com>

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -26,6 +26,15 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-compiler:2.44")
 
+    // room
+    val room_version = "2.4.3"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    kapt("android.arch.persistence.room:compiler:1.1.1")
+    testImplementation("androidx.room:room-testing:$room_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
