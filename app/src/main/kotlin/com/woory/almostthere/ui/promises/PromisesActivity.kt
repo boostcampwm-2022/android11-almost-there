d7ed52aeb7255c49bde79dea34754594ae870ebc
Yu Ho Hyeon <fbghgus123@naver.com>
2022-11-20 00:38:22 +0900

    :sparkles: feat: Promises 추가 및 이동
    - Promises Activity, Fragment 추가
    
    Issue #21

---
+++ app/src/main/kotlin/com/woory/almostthere/ui/promises/PromisesActivity.kt
@@ -0,0 +1,7 @@
+package com.woory.almostthere.ui.promises
+
+import com.woory.almostthere.R
+import com.woory.almostthere.databinding.ActivityPromisesBinding
+import com.woory.almostthere.ui.BaseActivity
+
+class PromisesActivity : BaseActivity<ActivityPromisesBinding>(R.layout.activity_promises)
---
+++ app/src/main/kotlin/com/woory/almostthere/ui/promises/PromisesFragment.kt
@@ -0,0 +1,25 @@
+package com.woory.almostthere.ui.promises
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.woory.almostthere.R
+import com.woory.almostthere.databinding.FragmentPromisesBinding
+import com.woory.almostthere.ui.BaseFragment
+
+class PromisesFragment: BaseFragment<FragmentPromisesBinding>(R.layout.fragment_promises) {
+
+    private val viewModel: PromisesViewModel by viewModels()
+    private val promisesAdapter = PromisesAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.rvPromises.adapter = promisesAdapter
+        promisesAdapter.submitList(viewModel.dummyPromises)
+    }
+}
