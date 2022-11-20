cf03ca593086b71fcf90c75d78bc0c5b887a094d
swjw13 <swjw13@gmail.com>
2022-11-21 01:30:05 +0900

    :recycle: refactor: 장소 검색 결과를 Map에 반양
    - Tmap의 검색 기능을 사용해 결과를 가져옴
    - 결과를 기준으로 Map의 위치를 옮겨줌
    
    Issue #21

--- app/src/main/kotlin/com/woory/almostthere/util/Constants.kt
+++ app/src/main/kotlin/com/woory/almostthere/util/Constants.kt
@@ -3,3 +3,4 @@ package com.woory.almostthere.util
 import com.woory.almostthere.BuildConfig
 
 const val MAP_API_KEY = BuildConfig.MAP_API_KEY
+const val REQUIRE_PERMISSION_TEXT = "Permission is required"
