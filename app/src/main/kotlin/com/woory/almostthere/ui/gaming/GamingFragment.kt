f43c063b996e4075f54a93ed670d5c768d02a8f4
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-11-20 20:23:14 +0900

    :lipstick: design: 게임 진행 화면 fragment ui 일부 구현
    - 필요한 icon 리소스 추가
    - 기본 UI (지역, 시간, 순위 리스트, 내 위치, 정보) 구현
    
    Issues #21

--- app/src/main/kotlin/com/woory/almostthere/ui/gaming/GamingFragment.kt
+++ app/src/main/kotlin/com/woory/almostthere/ui/gaming/GamingFragment.kt
@@ -1,9 +1,16 @@
 package com.woory.almostthere.ui.gaming
 
+import android.os.Bundle
+import android.view.View
 import com.woory.almostthere.R
 import com.woory.almostthere.databinding.FragmentGamingBinding
 import com.woory.almostthere.ui.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
+import com.woory.almostthere.util.MAP_API_KEY
 
-@AndroidEntryPoint
-class GamingFragment : BaseFragment<FragmentGamingBinding>(R.layout.fragment_gaming)
+class GamingFragment : BaseFragment<FragmentGamingBinding>(R.layout.fragment_gaming) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapGaming.setSKTMapApiKey(MAP_API_KEY)
+    }
+}
