4f30fa8406b17c11f6b22d92253d7fa26b1ab44d
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-11-20 20:18:30 +0900

    :recycle: refactor: MAP_API_KEY 위치 이동
    - MAP_API_KEY Util 로 이동
    
    Issues #21

--- app/src/main/kotlin/com/woory/almostthere/ui/creatingpromise/LocationSearchFragment.kt
+++ app/src/main/kotlin/com/woory/almostthere/ui/creatingpromise/LocationSearchFragment.kt
@@ -2,22 +2,16 @@ package com.woory.almostthere.ui.creatingpromise
 
 import android.os.Bundle
 import android.view.View
-import com.woory.almostthere.BuildConfig
 import com.woory.almostthere.R
 import com.woory.almostthere.databinding.FragmentLocationSearchBinding
 import com.woory.almostthere.ui.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
+import com.woory.almostthere.util.MAP_API_KEY
 
-@AndroidEntryPoint
 class LocationSearchFragment :
     BaseFragment<FragmentLocationSearchBinding>(R.layout.fragment_location_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mapPromiseLocationPick.setSKTMapApiKey(MAP_API_KEY)
-    }
-
-    companion object {
-        private const val MAP_API_KEY = BuildConfig.MAP_API_KEY
     }
 }
