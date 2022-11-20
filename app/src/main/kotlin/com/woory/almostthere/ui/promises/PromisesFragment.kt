c639d9d3f0f1c13a10f73db03af893734cd253d2
Yu Ho Hyeon <fbghgus123@naver.com>
2022-11-20 23:22:45 +0900

    :sparkles: feat: Gaming 화면 Bottom Sheet Layout 생성
    - bottomsheet_touch_character_marker 생성
    - Bottom Sheet에 style 적용
    
    Issue #21

--- app/src/main/kotlin/com/woory/almostthere/ui/promises/PromisesFragment.kt
+++ app/src/main/kotlin/com/woory/almostthere/ui/promises/PromisesFragment.kt
@@ -1,19 +1,25 @@
 package com.woory.almostthere.ui.promises
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.woory.almostthere.R
 import com.woory.almostthere.databinding.FragmentPromisesBinding
 import com.woory.almostthere.ui.BaseFragment
+import com.woory.almostthere.ui.gaming.GamingActivity
 
-class PromisesFragment: BaseFragment<FragmentPromisesBinding>(R.layout.fragment_promises) {
+class PromisesFragment : BaseFragment<FragmentPromisesBinding>(R.layout.fragment_promises) {
 
     private val viewModel: PromisesViewModel by viewModels()
     private val promisesAdapter = PromisesAdapter(
         onClickBefore = { },
-        onClickOngoing = { },
-        onClickEnd = {  },
+        onClickOngoing = {
+            // Todo :: 테스트용 화면 이동
+            val intent = Intent(requireActivity(), GamingActivity::class.java)
+            startActivity(intent)
+        },
+        onClickEnd = { },
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
