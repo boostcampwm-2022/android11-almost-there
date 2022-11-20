9a51fd820356041b23ae28801412e01126dd05ba
Yu Ho Hyeon <fbghgus123@naver.com>
2022-11-20 20:14:24 +0900

    :recycle: refactor: 필요없는 string resource 삭제
    - 필요없는 string resource 삭제
    - 필요없는 string resource 사용 중인 코드 삭제
    
    Issue #21

--- app/src/main/kotlin/com/woory/almostthere/ui/gameresult/GameResultActivity.kt
+++ app/src/main/kotlin/com/woory/almostthere/ui/gameresult/GameResultActivity.kt
@@ -8,9 +8,4 @@ import com.woory.almostthere.ui.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GameResultActivity : BaseActivity<ActivityGameResultBinding>(R.layout.activity_game_result) {
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-    }
-}
+class GameResultActivity : BaseActivity<ActivityGameResultBinding>(R.layout.activity_game_result)
