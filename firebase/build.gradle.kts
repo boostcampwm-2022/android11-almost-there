bd3464ba835ebd6572062b8f27782211295d02db
swjw13 <swjw13@gmail.com>
2022-11-22 10:54:08 +0900

    :recycle: refactor: gradle 파일 수정
    - 모듈별 종속성 순서 변경
    - ThreeTenABP 모듈을 네트워크 모듈, firebase 모듈에 저장
    
    Issue #32

--- firebase/build.gradle.kts
+++ firebase/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
     id("com.google.gms.google-services")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -13,14 +14,12 @@ android {
         minSdk = 23
         targetSdk = 33
     }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 dependencies {
-
-    // di
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-compiler:2.44")
-
     // Modules
     implementation(project(":data"))
 
@@ -29,6 +28,12 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // di
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+
+    implementation("com.jakewharton.threetenabp:threetenabp:1.4.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
