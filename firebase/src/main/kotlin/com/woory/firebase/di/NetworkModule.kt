45311e75e89d92076d517c91c95e5a8f9093771b
swjw13 <swjw13@gmail.com>
2022-11-22 10:54:58 +0900

    :hammer: feat: firebase hilt 모듈 추가
    - hilt 모듈 추가
    
    Issue #32

---
+++ firebase/src/main/kotlin/com/woory/firebase/di/NetworkModule.kt
@@ -0,0 +1,17 @@
+package com.woory.firebase.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideFireStoreDatabase(): FirebaseFirestore = Firebase.firestore
+}
