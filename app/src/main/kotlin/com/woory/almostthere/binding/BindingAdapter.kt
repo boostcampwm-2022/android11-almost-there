635bbc0cafa0961573f0d222ef8e0e8a35dda93f
soulplay95 <soulplay95@gmail.com>
2022-11-23 01:33:04 +0900

    :sparkles: feat: Firestore 관련 로직 구현
    - 약속 정보 가져오기 by 초대 코드
    
    Issue #7

--- app/src/main/kotlin/com/woory/almostthere/binding/BindingAdapter.kt
+++ app/src/main/kotlin/com/woory/almostthere/binding/BindingAdapter.kt
@@ -1,3 +1,38 @@
 package com.woory.almostthere.binding
 
-// BindingAdapter들을 최상위 함수로 모아두는 파일
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputLayout
+import com.woory.almostthere.R
+import com.woory.almostthere.ui.join.FormState
+
+@BindingAdapter("state_message")
+fun TextInputLayout.bindMessage(state: FormState) {
+    error = if (state is FormState.Invalid) {
+        context.getString(R.string.invalid_invite_code)
+    } else {
+        ""
+    }
+
+    helperText = if (state is FormState.Valid) {
+        context.getString(R.string.valid_invite_code)
+    } else {
+        ""
+    }
+}
+
+@BindingAdapter("enabled")
+fun Button.bindEnabled(state: FormState) {
+    isEnabled = state is FormState.Valid
+}
+
+@BindingAdapter("visibility")
+fun ProgressBar.bindVisibility(isGone: Boolean) {
+    visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
