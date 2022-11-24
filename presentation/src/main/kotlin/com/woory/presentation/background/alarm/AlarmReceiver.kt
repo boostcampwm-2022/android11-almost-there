7b69aeef0284875536368cd3223458d8f1038c8c
Yu Ho Hyeon <fbghgus123@naver.com>
2022-11-24 14:56:26 +0900

    :sparkles: 약속 생성 시 알람 등록
    - 약속 생성시 준비 알람 등록
    
    Issue #58

--- presentation/src/main/kotlin/com/woory/presentation/background/alarm/AlarmReceiver.kt
+++ presentation/src/main/kotlin/com/woory/presentation/background/alarm/AlarmReceiver.kt
@@ -17,19 +17,23 @@ class AlarmReceiver : BroadcastReceiver() {
         when (alarmType) {
             AlarmFunctions.PROMISE_READY -> {
                 if (promiseCode != null) {
-                    NotificationChannelProvider.providePromiseReadyChannel(context)
-
-                    // Todo :: 알람 메세지 수정 필요
-                    NotificationProvider.notifyPromiseReadyNotification(
-                        context,
-                        "임시 제목",
-                        "임시 소제목",
-                        promiseCode
-                    )
+                    onReceivePromiseReady(context, promiseCode)
                 }
             }
             AlarmFunctions.PROMISE_START -> {}
             else -> throw IllegalArgumentException("is not available alarm type")
         }
+    }
+
+    private fun onReceivePromiseReady(context: Context, promiseCode: String) {
+        NotificationChannelProvider.providePromiseReadyChannel(context)
+
+        // Todo :: 알람 메세지 수정 필요
+        NotificationProvider.notifyPromiseReadyNotification(
+            context,
+            "임시 제목",
+            "임시 소제목",
+            promiseCode
+        )
     }
 }
