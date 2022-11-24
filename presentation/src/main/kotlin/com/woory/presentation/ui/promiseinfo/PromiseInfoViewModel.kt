a55fbba39132a7a9fd06500f0efdafcbaca66b48
swjw13 <swjw13@gmail.com>
2022-11-24 13:10:02 +0900

    :hammer: feat: 약속 상세화면 구현
    - intent 로 약속 code를 이전 화면으로부터 받아온다.
    - code를 기반으로 서버에 데이터를 요청하고, 약속 정보를 불러온다.
    
    # Issue #27

--- presentation/src/main/kotlin/com/woory/presentation/ui/promiseinfo/PromiseInfoViewModel.kt
+++ presentation/src/main/kotlin/com/woory/presentation/ui/promiseinfo/PromiseInfoViewModel.kt
@@ -1,5 +1,71 @@
 package com.woory.presentation.ui.promiseinfo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woory.data.repository.PromiseRepository
+import com.woory.presentation.model.PromiseDataModel
+import com.woory.presentation.model.mapper.PromiseDataModelMapper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PromiseInfoViewModel : ViewModel()
+@HiltViewModel
+class PromiseInfoViewModel @Inject constructor(
+    private val repository: PromiseRepository
+) : ViewModel() {
+    private val _gameCode: MutableStateFlow<String> = MutableStateFlow("")
+    private val gameCode: StateFlow<String> = _gameCode.asStateFlow()
+
+    private val _isMapReady: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isMapReady: StateFlow<Boolean> = _isMapReady.asStateFlow()
+
+    private val _uiState: MutableStateFlow<PromiseUiState> =
+        MutableStateFlow(PromiseUiState.Waiting)
+    val uiState: StateFlow<PromiseUiState> = _uiState.asStateFlow()
+
+    private val _errorState: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val errorState: StateFlow<Boolean> = _errorState.asStateFlow()
+
+    private val _promiseModel: MutableStateFlow<PromiseDataModel?> = MutableStateFlow(null)
+    val promiseModel: StateFlow<PromiseDataModel?> = _promiseModel.asStateFlow()
+
+    fun setGameCode(code: String) {
+        viewModelScope.launch {
+            _gameCode.emit(code)
+        }
+    }
+
+    fun setMapReady(isMapReady: Boolean) {
+        viewModelScope.launch {
+            _isMapReady.emit(isMapReady)
+        }
+    }
+
+    fun setUiState(state: PromiseUiState) {
+        viewModelScope.launch {
+            _uiState.emit(state)
+        }
+    }
+
+    fun setErrorState(boolean: Boolean){
+        viewModelScope.launch {
+            _errorState.emit(boolean)
+        }
+    }
+
+    fun fetchPromiseDate() {
+        viewModelScope.launch {
+            val code = gameCode.value
+            _uiState.emit(PromiseUiState.Loading)
+            repository.getPromiseByCode(code).onSuccess {
+                _promiseModel.emit(PromiseDataModelMapper.asUiState(it))
+                _uiState.emit(PromiseUiState.Success)
+            }.onFailure {
+                setErrorState(true)
+            }
+        }
+    }
+}
