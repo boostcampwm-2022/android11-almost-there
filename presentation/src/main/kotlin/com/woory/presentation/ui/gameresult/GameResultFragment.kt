59537c5476f42da9e6aa4db503d80fc06a4abc75
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-11-29 23:43:37 +0900

    :lipstick: design: UserRankingAdapter 구현 및 dummy 데이터로 submitList
    - UserRankingAdapter 구현
    - ViewModel 에서 dummy 데이터로 submitList 실행
    - 결과 화면 일부 디자인 변경
    
    Issue #81

--- presentation/src/main/kotlin/com/woory/presentation/ui/gameresult/GameResultFragment.kt
+++ presentation/src/main/kotlin/com/woory/presentation/ui/gameresult/GameResultFragment.kt
@@ -3,11 +3,16 @@ package com.woory.presentation.ui.gameresult
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.woory.presentation.R
 import com.woory.presentation.databinding.FragmentGameResultBinding
 import com.woory.presentation.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class GameResultFragment : BaseFragment<FragmentGameResultBinding>(R.layout.fragment_game_result) {
@@ -18,6 +23,8 @@ class GameResultFragment : BaseFragment<FragmentGameResultBinding>(R.layout.frag
         super.onViewCreated(view, savedInstanceState)
         setUpToolbar()
         setUpClickListener()
+        setUpAdapter()
+        observeData()
     }
 
     private fun setUpToolbar() {
@@ -31,6 +38,20 @@ class GameResultFragment : BaseFragment<FragmentGameResultBinding>(R.layout.frag
 
         binding.btnExit.setOnClickListener {
             requireActivity().finish()
+        }
+    }
+
+    private fun setUpAdapter() {
+        binding.rvRanking.adapter = UserRankingAdapter()
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.userRankingList.collectLatest {
+                    (binding.rvRanking.adapter as UserRankingAdapter).submitList(it)
+                }
+            }
         }
     }
 }
--- presentation/src/main/kotlin/com/woory/presentation/ui/gameresult/GameResultViewModel.kt
+++ presentation/src/main/kotlin/com/woory/presentation/ui/gameresult/GameResultViewModel.kt
@@ -1,10 +1,52 @@
 package com.woory.presentation.ui.gameresult
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woory.presentation.model.UserData
+import com.woory.presentation.model.UserProfileImage
+import com.woory.presentation.model.UserRanking
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GameResultViewModel @Inject constructor(): ViewModel() {
 
+    private val _userRankingList: MutableStateFlow<List<UserRanking>> = MutableStateFlow(emptyList())
+    val userRankingList: StateFlow<List<UserRanking>> = _userRankingList.asStateFlow()
+
+    init {
+        fetchUserRankingList()
+    }
+
+    private fun fetchUserRankingList() {
+        viewModelScope.launch {
+            // TODO("테스트값")
+            val testList = listOf(UserRanking(
+                userId = "1",
+                userData = UserData("수진", UserProfileImage("#121212", 0)),
+                85,
+                1
+            ), UserRanking(
+                userId = "2",
+                userData = UserData("재우", UserProfileImage("#ff0000", 1)),
+                50,
+                1
+            ), UserRanking(
+                userId = "3",
+                userData = UserData("호현", UserProfileImage("#ff00ff", 2)),
+                20,
+                1
+            ), UserRanking(
+                userId = "4",
+                userData = UserData("도명", UserProfileImage("#00ffff", 3)),
+                10,
+                1
+            ))
+            _userRankingList.emit(testList)
+        }
+    }
 }
