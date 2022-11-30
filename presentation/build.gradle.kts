f0e824be48133844b9fd0a9258eeabb5991a74e6
swjw13 <swjw13@gmail.com>
2022-11-30 21:11:51 +0900

    :wrench: chore: build.gradle 추가
    - FusedLocationProvider 라이브러리 추가
    
    Issue #68

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -85,6 +85,9 @@ dependencies {
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
 
+    // FusedLocationProvider
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
