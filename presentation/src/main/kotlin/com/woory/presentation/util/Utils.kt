4a3be5084a6e479a460f1af90490ceac56c462cc
Yu Ho Hyeon <fbghgus123@naver.com>
2022-12-01 22:54:12 +0900

    :sparkles: feat: 좌표 -> m(미터) 변환 코드 작성
    Issue #99

--- presentation/src/main/kotlin/com/woory/presentation/util/Utils.kt
+++ presentation/src/main/kotlin/com/woory/presentation/util/Utils.kt
@@ -4,7 +4,13 @@ import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import androidx.navigation.NavOptions
 import com.woory.presentation.R
+import com.woory.presentation.model.GeoPoint
 import java.text.DecimalFormat
+import kotlin.math.atan
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 val animRightToLeftNavOption = NavOptions.Builder().apply {
     setEnterAnim(R.anim.slide_in_from_right)
@@ -28,6 +34,22 @@ val textScaleAnimation = ScaleAnimation(
     repeatCount = Animation.INFINITE
     duration = 1000
     repeatMode = Animation.REVERSE
+}
+
+fun getDistance(start: GeoPoint, end: GeoPoint): Double {
+    val sLat = start.latitude
+    val sLon = start.longitude
+    val eLat = end.latitude
+    val eLon = end.longitude
+
+    val earthRadius = 6378.137
+    val dLat = eLat * Math.PI / 180 - sLat * Math.PI / 180
+    val dLon = eLon * Math.PI / 180 - sLon * Math.PI / 180
+    val a = sin(dLat / 2) * sin(dLat / 2) +
+            cos(sLat * Math.PI / 180) * cos(dLat * Math.PI / 180) * sin(dLon / 2) * sin(dLon / 2)
+    val c = 2 * atan2(sqrt(a), sqrt(1-a))
+    val d = earthRadius * c
+    return d * 1000
 }
 
 fun getHex(value: Int): String = "%02X".format(value)
