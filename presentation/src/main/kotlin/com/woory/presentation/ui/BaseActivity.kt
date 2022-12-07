ec13b6370d554f868da03316dc6be183a3d4261b
soulplay95 <soulplay95@gmail.com>
2022-12-08 01:36:00 +0900

    :hammer: feat: Toolbar 셋업 코드 BaseActivity로 이동
    - + lifecycleOwner
    
    Issue #121

--- presentation/src/main/kotlin/com/woory/presentation/ui/BaseActivity.kt
+++ presentation/src/main/kotlin/com/woory/presentation/ui/BaseActivity.kt
@@ -3,15 +3,30 @@ package com.woory.presentation.ui
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
     AppCompatActivity() {
-    lateinit var binding: T
+
+    protected lateinit var binding: T
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, layoutResId)
+
+        binding = DataBindingUtil.setContentView<T>(this, layoutResId).apply {
+            lifecycleOwner = this@BaseActivity
+        }
+    }
+
+    protected fun initToolbar(toolbar: Toolbar, toolbarTitle: String) = with(toolbar) {
+        setSupportActionBar(this)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            title = toolbarTitle
+        }
     }
 }
