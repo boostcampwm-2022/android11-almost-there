e26e995fbc2171d3c159074f66375257c52dccd6
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-12-08 18:11:05 +0900

    :bug: fix: 이전 테스트 코드 버그 수정
    Issue #146

--- network/src/test/kotlin/com/woory/network/fake/FakeNetworkDataSource.kt
+++ network/src/test/kotlin/com/woory/network/fake/FakeNetworkDataSource.kt
@@ -1,6 +1,7 @@
 package com.woory.network.fake
 
 import com.woory.data.model.GeoPointModel
+import com.woory.data.model.LocationSearchModel
 import com.woory.data.model.PathModel
 import com.woory.data.source.NetworkDataSource
 import com.woory.network.TMapService
@@ -13,7 +14,25 @@ class FakeNetworkDataSource(private val service: TMapService): NetworkDataSource
         }
     }
 
-    override suspend fun getPath(start: GeoPointModel, dest: GeoPointModel): Result<PathModel> {
+    override suspend fun getCarRoute(start: GeoPointModel, dest: GeoPointModel): Result<PathModel> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPublicTransitRoute(
+        start: GeoPointModel,
+        dest: GeoPointModel
+    ): Result<PathModel> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getWalkRoute(
+        start: GeoPointModel,
+        dest: GeoPointModel
+    ): Result<PathModel> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchLocationByKeyword(keyword: String): Result<List<LocationSearchModel>> {
         TODO("Not yet implemented")
     }
 }
