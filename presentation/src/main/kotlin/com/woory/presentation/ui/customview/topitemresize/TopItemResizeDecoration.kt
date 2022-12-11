d0bd65dadd3041ef707bd94689f6ed804f92af39
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2022-12-12 02:11:34 +0900

    :bug: fix: TopItemResizeRecyclerView 버그 수정
    Issue #184

--- presentation/src/main/kotlin/com/woory/presentation/ui/customview/topitemresize/TopItemResizeDecoration.kt
+++ presentation/src/main/kotlin/com/woory/presentation/ui/customview/topitemresize/TopItemResizeDecoration.kt
@@ -2,6 +2,7 @@ package com.woory.presentation.ui.customview.topitemresize
 
 import android.graphics.Rect
 import android.view.View
+import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,13 +15,15 @@ class TopItemResizeDecoration : RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
-        if (parent.getChildAdapterPosition(view) + 1 == parent.adapter?.itemCount) {
+        if (parent.getChildAdapterPosition(view) + 1 == parent.adapter?.itemCount && parent.childCount != 1) {
             when (val layoutManager = parent.layoutManager) {
                 is LinearLayoutManager -> {
                     if (layoutManager.orientation == RecyclerView.VERTICAL) {
-                        outRect.bottom = parent.height - view.height
+                        val highlightViewHeight = parent.children.maxOf { it.height }
+                        outRect.bottom = parent.height - highlightViewHeight - parent.paddingTop - parent.paddingBottom
                     } else {
-                        outRect.right = parent.width - view.width
+                        val highlightViewWidth = parent.children.maxOf { it.width }
+                        outRect.right = parent.width - highlightViewWidth - parent.paddingStart - parent.paddingEnd
                     }
                 }
                 else -> throw IllegalArgumentException("TopItemResizeDecoration 은 LinearLayoutManager 일 때만 지원합니다.")
