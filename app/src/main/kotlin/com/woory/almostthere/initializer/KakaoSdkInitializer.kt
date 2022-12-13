fa2c6def6aad87bd1f47828bcc3604ffe40c95df
soulplay95 <soulplay95@gmail.com>
2022-12-13 20:09:47 +0900

    :lipstick: design: Format 수정
    Issue #215

--- app/src/main/kotlin/com/woory/almostthere/initializer/KakaoSdkInitializer.kt
+++ app/src/main/kotlin/com/woory/almostthere/initializer/KakaoSdkInitializer.kt
@@ -5,7 +5,8 @@ import androidx.startup.Initializer
 import com.kakao.sdk.common.KakaoSdk
 import com.woory.almostthere.BuildConfig
 
-class KakaoSdkInitializer: Initializer<Unit> {
+class KakaoSdkInitializer : Initializer<Unit> {
+
     override fun create(context: Context) {
         KakaoSdk.init(context, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
