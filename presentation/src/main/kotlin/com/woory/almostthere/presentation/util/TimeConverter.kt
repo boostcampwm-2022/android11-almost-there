abe06bd2992c8c2fe29971ca9159676c3db86347
swjw13 <swjw13@gmail.com>
2022-12-15 22:42:04 +0900

    :recycle: refactor: 와일드 카드 제거
    - import 문 와일드카드 제거
    
    Pull Request #228

--- presentation/src/main/kotlin/com/woory/almostthere/presentation/util/TimeConverter.kt
+++ presentation/src/main/kotlin/com/woory/almostthere/presentation/util/TimeConverter.kt
@@ -4,13 +4,11 @@ import org.threeten.bp.Instant
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZoneOffset
-import java.util.*
 
 object TimeConverter {
 
     private val zoneId: ZoneId = ZoneId.of("Asia/Seoul")
     val zoneOffset: ZoneOffset = ZoneOffset.of("+09:00")
-    private val locale: Locale = Locale.KOREA
 
     fun OffsetDateTime.asMillis() = toInstant().toEpochMilli()
 
